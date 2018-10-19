8592f1b5258081e86b4e522c103f16be2a969626
Min Hu <minhuvsp@gmail.com>
2018-10-19 15:15:23 -0700

    Initial commit

---
+++ src/main/java/com/vsp/api/test/apitestspring/ApiTestSpringApplication.java
@@ -0,0 +1,12 @@
+package com.vsp.api.test.apitestspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTestSpringApplication.class, args);
+	}
+}
